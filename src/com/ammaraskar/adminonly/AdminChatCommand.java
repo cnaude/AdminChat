e4d46d4689812b82bcfea545b159441e430550af
Ammar Askar <ammar_askar@hotmail.com>
2012-04-04 22:38:31 +0500

    Sanity check.

--- src/com/ammaraskar/adminonly/AdminChatCommand.java
+++ src/com/ammaraskar/adminonly/AdminChatCommand.java
@@ -1,5 +1,6 @@
 package com.ammaraskar.adminonly;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -12,9 +13,13 @@ public class AdminChatCommand implements CommandExecutor {
     
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender.hasPermission("adminchat.send") || sender.isOp()){
-        		String playername = sender.getName();
-        		final String message = com.ammaraskar.adminonly.Methods.combineSplit(0, args, " ");
-        		methods.MessageBuild(message, playername);
+            if (args.length < 1) {
+                sender.sendMessage(ChatColor.RED + "Usage: /a message");
+                return false;
+            }
+        	String playername = sender.getName();
+        	final String message = com.ammaraskar.adminonly.Methods.combineSplit(0, args, " ");
+        	methods.MessageBuild(message, playername);
         }
         return true;
     }
