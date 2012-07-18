1998310f149f3f625cf008b41968101f0b3b7887
Ammar Askar <ammar_askar@hotmail.com>
2012-07-18 16:38:11 +0100

    Doing /amsg or /a while toggled into admin chat will now send to normal chat

--- src/com/ammaraskar/adminonly/AdminChatCommand.java
+++ src/com/ammaraskar/adminonly/AdminChatCommand.java
@@ -1,13 +1,20 @@
 package com.ammaraskar.adminonly;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerChatEvent;
 
 public class AdminChatCommand implements CommandExecutor {
+    
 	Methods methods;
+	AdminChat plugin;
+	
     public AdminChatCommand(AdminChat adminchat) {
+        this.plugin = adminchat;
     	this.methods = adminchat.methods;
     }
     
@@ -15,10 +22,34 @@ public class AdminChatCommand implements CommandExecutor {
         if (sender.hasPermission("adminchat.send") || sender.isOp()){
             if (args.length < 1) {
                 sender.sendMessage(ChatColor.RED + "Usage: /a message");
-                return false;
+                return true;
             }
         	String playername = sender.getName();
-        	final String message = com.ammaraskar.adminonly.Methods.combineSplit(0, args, " ");
+            final String message = Methods.combineSplit(0, args, " ");
+            
+        	if (plugin.listOfTogglePlayers.contains(playername)) {
+        	    Player player;
+        	    if(sender instanceof Player) {
+        	        player = (Player) sender;
+        	    } else {
+        	        sender.sendMessage(ChatColor.RED + "You aren't supposed to be here!");
+        	        return true;
+        	    }
+                PlayerChatEvent event = new PlayerChatEvent(player, message);
+                plugin.getServer().getPluginManager().callEvent(event);
+
+                if (event.isCancelled()) {
+                    return true;
+                }
+
+                String s = String.format(event.getFormat(), event.getPlayer().getDisplayName(), event.getMessage());
+                Bukkit.getConsoleSender().sendMessage(s);
+                for (Player recipient : event.getRecipients()) {
+                    recipient.sendMessage(s);
+                }
+                return true;
+        	}
+        	
         	methods.MessageBuild(message, playername);
         }
         return true;
