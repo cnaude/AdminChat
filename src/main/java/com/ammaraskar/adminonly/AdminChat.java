2c548669ac931054ed5f01737aa1512c5fe34f64
cnaude <cnaude@zebra.lan>
2016-03-13 18:48:06 -0700

    Update pom with MC 1.9.

--- src/main/java/com/ammaraskar/adminonly/AdminChat.java
+++ src/main/java/com/ammaraskar/adminonly/AdminChat.java
@@ -3,6 +3,7 @@ package com.ammaraskar.adminonly;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.logging.Level;
 
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.PluginDescriptionFile;
@@ -13,12 +14,16 @@ public class AdminChat extends JavaPlugin {
 
     public String format;
     public Methods methods;
-    public Set<String> toggledPlayers = Collections.synchronizedSet(new HashSet<String>());
+    public Set<String> toggledPlayers;
+
+    public AdminChat() {
+        this.toggledPlayers = Collections.synchronizedSet(new HashSet<String>());
+    }
 
     @Override
     public void onDisable() {
         PluginDescriptionFile pdfFile = this.getDescription();
-        this.getLogger().info("AdminChat " + pdfFile.getVersion() + " disabled.");
+        this.getLogger().log(Level.INFO, "AdminChat {0} disabled.", pdfFile.getVersion());
     }
 
     @Override
@@ -32,7 +37,7 @@ public class AdminChat extends JavaPlugin {
         this.getCommand("atoggle").setExecutor(new AdminChatToggleCommand(this));
         
         this.format = methods.SubstituteColors(this.getConfig().getString("format"));
-        this.getLogger().info("Using format: " + format);
+        this.getLogger().log(Level.INFO, "Using format: {0}", format);
         
         if (!format.contains("%playername") && !format.contains("%message")) {
             this.getLogger().severe("Format did not contain %playername or %message, switching to fallback format");
